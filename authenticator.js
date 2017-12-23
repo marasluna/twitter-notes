86e55306e0b3c4c320aa213b2e8a7368a492f1e3
marasluna <marasluna@gmail.com>
2017-12-22 17:45:58 -0800

    RESTful Web API with Node.js
    A Twitter notes application

---
+++ authenticator.js
@@ -0,0 +1,42 @@
+var OAuth = require('oauth').OAuth;
+var config = require('./config');
+
+//  Ceate the oath object for accessing Twitter
+var oauth = new OAuth(
+	config.request_token_url,
+	config.access_token_url,
+	config.consumer_key,
+	config.consumer_secret,
+	config.oauth_version,
+	config.oauth_callback,
+	config.oauth_signature
+);
+
+module.exports = {
+	redirectToTwitterLoginPage: function(req, res){
+		//Ask Twitter for a request token
+		oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
+			if(error) {
+				console.log(error);
+				res.send("Authentication failed");
+			} else {
+				res.cookie('oauth_token', oauth_token, {httpOnly: true});
+				res.cookie('oauth_token_secret', oauth_token_secret, {httpOnly: true});
+				res.redirect(config.authorize_url + '?oauth_token=' + oauth_token);
+			}
+		});
+	},
+	authenticate: function(req, res, cb) {
+		// Check if the request token and temporary credential are there
+		if (!(req.cookies.oauth_token && req.cookies.oauth_token_secret && req.query.oauth_verifier)) {
+			return cb("Request does not have all require keys");
+		}
+
+		// Clear request token cookie
+		res.clearCookie('oauth_token');
+		res.clearCookie('oauth_token_secret');
+
+		//Rell the router that authentication was successful
+		cb();
+	}
+}
